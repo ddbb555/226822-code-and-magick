5fa40c9e431f95e19e22553e351a5c823b54d882
vladimir smirnov <mnblkj567@gmail.com>
2017-01-26 15:54:49 +0300

    #8 Учебный проект: Холст ( добавил константы )

--- js/stat.js
+++ js/stat.js
@@ -8,15 +8,27 @@ window.renderStatistics = function (ctx, names, times) {
   var histoX = 140; // Х гистрограммы
   var maxColumnHeight = Math.max.apply(null, times);
   var step = histoHeight / maxColumnHeight;
+
+
+
+  var FILL_OF_SHADOW = 'rgba(0, 0, 0, 0.7)';
+  var FILL_OF_WINDOW = 'rgba(256, 256, 256, 1.0)';
+  var FONT_COLOR = '#000';
+  var FONT_SIZE = '16px PT Mono';
+  var COLOR_RED = 'rgba(255, 0, 0, 1)';
+  var COLOR_RANDOM_BLUE = ['rgba(0,0,', ((Math.random() * 5) * 50).toFixed(0), ',1)'].join('');
+
+
+
   
 
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillStyle = FILL_OF_SHADOW;
   ctx.fillRect(110, 20, 420, 270);
-  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
+  ctx.fillStyle = FILL_OF_WINDOW;
   ctx.strokeRect(100, 10, 420, 270);
   ctx.fillRect(100, 10, 420, 270);
-  ctx.fillStyle = '#000';
-  ctx.font = '16px Mono';
+  ctx.fillStyle = FONT_COLOR;
+  ctx.font = FONT_SIZE;
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
@@ -26,9 +38,9 @@ window.renderStatistics = function (ctx, names, times) {
     var columnHeight = step * time;
 
     if (name === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      ctx.fillStyle = COLOR_RED;
     } else {
-      ctx.fillStyle = ['rgba(0,0,', ((Math.random() * 5) * 50).toFixed(0), ',1)'].join('');
+      ctx.fillStyle = COLOR_RANDOM_BLUE;
     }
 
     ctx.fillRect(histoX + columnIndent * i, 100 + histoHeight - columnHeight, columnWidth, columnHeight);
