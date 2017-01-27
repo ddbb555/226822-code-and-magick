f08f6c242eb0b0a5f42c57c82ab01125e2ec7d96
vladimir smirnov <mnblkj567@gmail.com>
2017-01-27 19:25:24 +0300

    последние правки

--- js/stat.js
+++ js/stat.js
@@ -9,28 +9,32 @@ window.renderStatistics = function (ctx, names, times) {
   var maxColumnHeight = Math.max.apply(null, times);
   var step = histoHeight / maxColumnHeight;
 
-
-
   var FILL_OF_SHADOW = 'rgba(0, 0, 0, 0.7)';
   var FILL_OF_WINDOW = 'rgba(256, 256, 256, 1.0)';
   var FONT_COLOR = '#000';
   var FONT_SIZE = '16px PT Mono';
   var COLOR_RED = 'rgba(255, 0, 0, 1)';
-  var COLOR_RANDOM_BLUE = ['rgba(0,0,', ((Math.random() * 5) * 50).toFixed(0), ',1)'].join('');
 
-
+  var shadowRect = { top: 110, bottom: 20, left: 420, right: 270 };
+  var windowRect = { top: 100, bottom: 10, left: 420, right: 270 };
+  var windowStrokeRect = { top: 100, bottom: 10, left: 420, right: 270 };
+  var youWonText = { text: 'Ура вы победили!', x: 120, y: 40 };
+  var resultText = { text: 'Список результатов:', x: 120, y: 60 };
 
   
-
   ctx.fillStyle = FILL_OF_SHADOW;
-  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillRect(shadowRect.top, shadowRect.bottom, shadowRect.left, shadowRect.right);
   ctx.fillStyle = FILL_OF_WINDOW;
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
+  ctx.strokeRect(windowStrokeRect.top, windowStrokeRect.bottom, windowStrokeRect.left, windowStrokeRect.right);
+  ctx.fillRect(windowRect.top, windowRect.bottom, windowRect.left, windowRect.right);
   ctx.fillStyle = FONT_COLOR;
   ctx.font = FONT_SIZE;
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
+  ctx.fillText(youWonText.text, youWonText.x, youWonText.y);
+  ctx.fillText(resultText.text, resultText.x, resultText.y);
+
+  function getRandomBlueColor() {
+   return ['rgba(0,0,', ((Math.random() * 5) * 50).toFixed(0), ',1)'].join('');
+   }
 
   for (var i = 0; i < times.length; i++) {
     var name = names[i];
@@ -40,7 +44,7 @@ window.renderStatistics = function (ctx, names, times) {
     if (name === 'Вы') {
       ctx.fillStyle = COLOR_RED;
     } else {
-      ctx.fillStyle = COLOR_RANDOM_BLUE;
+      ctx.fillStyle = getRandomBlueColor();
     }
 
     ctx.fillRect(histoX + columnIndent * i, 100 + histoHeight - columnHeight, columnWidth, columnHeight);
