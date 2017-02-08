d717bdc77453e9e9806c0f643f9823b5d6444ced
vladimir smirnov <mnblkj567@gmail.com>
2017-02-08 19:44:33 +0300

    Задание 13

--- js/setup.js
+++ js/setup.js
@@ -3,7 +3,7 @@
 var setupOpen = document.querySelector('.setup-open');
 var setup = document.querySelector('.setup');
 var setupClose = setup.querySelector('.setup-close');
-var setupUserName = setup.querySelector('.setup-user-name');
+
 
 setupOpen.addEventListener('click', function () {
   setup.classList.remove('invisible');
@@ -60,3 +60,52 @@ wizardEyes.addEventListener('click', function () {
 fireball.addEventListener('click', function () {
   fireball.style.backgroundColor = getElementColor(fireballColors);
 });
+
+
+
+
+var ENTER_KEY_CODE = 13;
+var ESC_KEY_CODE = 27;
+
+function enterPressed(evt) {
+  return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
+}
+
+function setupEscEvent(evt) {
+  if (evt.keyCode && evt.keyCode === ESC_KEY_CODE) {
+    setup.classList.add('invisible');
+  }
+}
+
+function toggleSetup() {
+  if (setup.classList.contains('invisible')) {
+    setup.classList.remove('invisible');
+    document.addEventListener('keydown', setupEscEvent);
+    setupOpen.setAttribute('aria-pressed', true);
+    setup.setAttribute('aria-hidden', false);
+    setupClose.setAttribute('aria-hidden', false);
+  }
+  else {
+    setup.classList.add('invisible');
+    document.removeEventListener('keydown', setupEscEvent);
+    setupOpen.setAttribute('aria-pressed', false);
+    setup.setAttribute('aria-hidden', true);
+    setupClose.setAttribute('aria-hidden', true);
+  }
+}
+
+setupOpen.addEventListener('keydown', function(evt) {
+  if (enterPressed(evt)) {
+    toggleSetup();
+  }
+});
+
+setupClose.addEventListener('keydown', function(evt) {
+  if (enterPressed(evt)) {
+    toggleSetup();
+  }
+});
+
+
+
+
